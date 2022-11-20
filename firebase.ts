fa14e77b1d82eba4309e03c0af74d3ee88ecd9f3
zerg41 <zerg41@gmail.com>
2022-11-20 21:53:33 +0300

    dev: init CRA; added firebase

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA9rO1oYRSgqX-FGDMstaq8pgLJsWnCJlM',
+  authDomain: 'todolist-app-f7fc4.firebaseapp.com',
+  projectId: 'todolist-app-f7fc4',
+  storageBucket: 'todolist-app-f7fc4.appspot.com',
+  messagingSenderId: '93736607640',
+  appId: '1:93736607640:web:5f3de12e45298d6193d5ed',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// components
+import App from 'app';
+// style
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
